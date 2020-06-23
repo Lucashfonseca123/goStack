a251ea08656e0e71df6d9ecb7b93ceff6db3fcab
lucas.fonseca <lucas.fonseca@trizy.com.br>
2020-06-23 01:15:59 -0300

    Iniciado agendamentos

--- goBarber/06-gobarber-web/src/pages/Dashboard/index.tsx
+++ goBarber/06-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,75 @@
 import React from 'react';
 
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Schedule,
+  Content,
+  Calendar,
+  NextAppointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { signOut, user } = useAuth();
+
+  console.log(user);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img
+              src="https://avatars2.githubusercontent.com/u/32250723?s=460&u=a09113cd70b1d2ce5677e2950467715a6376d2fa&v=4"
+              alt={user.name}
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horário agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/32250723?s=460&u=a09113cd70b1d2ce5677e2950467715a6376d2fa&v=4"
+                alt="Imagem"
+              />
+              <strong>Lucas Fonseca</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
