94c7b0c7c0be05cb34a0b16837acc3b75882bd52
lucas.fonseca <lucas.fonseca@trizy.com.br>
2020-06-25 22:54:33 -0300

    Finalizado dashboard, iniciado página cabeleireiros

--- goBarber/appgobarber/src/pages/CreateAppointment/index.tsx
+++ goBarber/appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+
+import Icon from 'react-native-vector-icons/Feather';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const { providerId } = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar
+          source={{
+            uri:
+              'https://avatars2.githubusercontent.com/u/32250723?s=460&u=a09113cd70b1d2ce5677e2950467715a6376d2fa&v=4',
+          }}
+        />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
--- goBarber/appgobarber/src/pages/Dashboard/index.tsx
+++ goBarber/appgobarber/src/pages/Dashboard/index.tsx
@@ -1,14 +1,106 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProviderList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+  ProvidersListTitle,
+} from './styles';
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointments', { providerId });
+    },
+    [navigate],
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button onPress={signOut} title="Sair" />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem-vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar
+            source={{
+              uri:
+                'https://avatars2.githubusercontent.com/u/32250723?s=460&u=a09113cd70b1d2ce5677e2950467715a6376d2fa&v=4',
+            }}
+          />
+        </ProfileButton>
+      </Header>
+
+      <ProviderList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <ProviderAvatar
+              source={{
+                uri:
+                  'https://avatars2.githubusercontent.com/u/32250723?s=460&u=a09113cd70b1d2ce5677e2950467715a6376d2fa&v=4',
+              }}
+            />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderMetaText>08h as 16h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
+    </Container>
   );
 };
 
